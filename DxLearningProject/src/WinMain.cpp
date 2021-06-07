b82e073d39d7009dfa7b00d1a134796816fda2f1
Lasse Siniluoto <c6sila00@students.oamk.fi>
2021-06-07 21:52:08 +0300

    Added Window class

--- DxLearningProject/src/WinMain.cpp
+++ DxLearningProject/src/WinMain.cpp
@@ -1,48 +1,12 @@
-#include <Windows.h>
+#include "Window.h"
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
-{
-    switch (msg)
-    {
-    case WM_CLOSE:
-        PostQuitMessage(69);
-        break;
-    }
-    return DefWindowProc(hWnd, msg, wParam, lParam);
-}
-
-int CALLBACK WinMain(
+int WINAPI WinMain(
     HINSTANCE hInstance,
     HINSTANCE hPrevInstance,
     PSTR lpCmdLine,
     int nCmdShow )
 {
-    const wchar_t pClassName[] = L"DxLearningProject";
-    // register windows class
-    WNDCLASSEX wc = { 0 };
-    wc.cbSize = sizeof(wc);
-    wc.style = CS_OWNDC;
-    wc.lpfnWndProc = WndProc;
-    wc.cbClsExtra = 0;
-    wc.cbWndExtra = 0;
-    wc.hInstance = hInstance;
-    wc.hIcon = nullptr;
-    wc.hCursor = nullptr;
-    wc.hbrBackground = nullptr;
-    wc.lpszMenuName = nullptr;
-    wc.lpszClassName = pClassName;
-    wc.hIconSm = nullptr;
-    RegisterClassEx( &wc );
-    // Create window instance
-    HWND hWnd = CreateWindowEx(
-        0, pClassName,
-        L"Happy Hard Window",
-        WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
-        200, 200, 640, 480,
-        nullptr, nullptr, hInstance, nullptr
-    );
-    // Show damn window
-    ShowWindow(hWnd, SW_SHOW);
+    Window wnd(800, 300, L"Test Program");
 
     // message pump
     MSG msg;
---
+++ DxLearningProject/src/Window.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "windows_helper.h"
+class Window
+{
+private:
+	class WindowClass
+	{
+	public:
+		static const wchar_t* GetName() noexcept;
+		static HINSTANCE GetInstance() noexcept;
+	private:
+		WindowClass() noexcept;
+		~WindowClass();
+		WindowClass(const WindowClass&) = delete;
+		WindowClass& operator=(const WindowClass&) = delete;
+		static constexpr const wchar_t* wndClassName = L"DxLearningProject";
+		static WindowClass wndClass;
+		HINSTANCE hInst;
+	};
+public:
+	Window(int width, int height, const wchar_t* name) noexcept;
+	~Window();
+	Window(const Window&) = delete;
+	Window& operator=(const Window&) = delete;
+private:
+	static LRESULT WINAPI HandleMsgSetup(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept;
+	static LRESULT WINAPI HandleMsgThunk(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept;
+	LRESULT HandleMsg(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept;
+private:
+	int width;
+	int height;
+	HWND hWnd;
+};
+
